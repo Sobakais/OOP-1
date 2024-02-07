bbc365ea868497f5c7fcb16d9f8e7479684e3964
Sobakais <ugoneid@gmail.com>
2024-02-07 22:36:59 +0300

    Added getCentre() function

--- Rectangle/main.cpp
+++ Rectangle/main.cpp
@@ -1,11 +1,21 @@
 #include "rectangle.hpp"
 #include <iostream>
+#include <utility>
 
 int main() {
   Rectangle test1;
   Rectangle test2(2.3, 1.5);
   Rectangle test3(4.2, 3.1, 2.0, 3.0);
+  std::pair<double, double> centre1 = test1.getCentre();
+  std::pair<double, double> centre2 = test2.getCentre();
+  std::pair<double, double> centre3 = test3.getCentre();
   std::cout << "Area of first Rectangle: " << test1.getArea() << std::endl;
+  std::cout << "Center of first rectangle is X: " << centre1.first
+            << " Y: " << centre1.second << std::endl;
   std::cout << "Area of second Rectangle: " << test2.getArea() << std::endl;
+  std::cout << "Center of first rectangle is X: " << centre2.first
+            << " Y: " << centre2.second << std::endl;
   std::cout << "Area of third Rectangle: " << test3.getArea() << std::endl;
+  std::cout << "Center of first rectangle is X: " << centre3.first
+            << " Y: " << centre3.second << std::endl;
 }
--- Rectangle/rectangle.hpp
+++ Rectangle/rectangle.hpp
@@ -1,6 +1,7 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include <utility>
 class Rectangle {
 public:
   Rectangle(double Horiz, double Vert) {
@@ -15,6 +16,9 @@ public:
   }
   Rectangle() = default;
   double getArea() { return lenghtHorisontal * lenghtVertical; }
+  std::pair<double, double> getCentre() {
+    return std::make_pair(x + (lenghtHorisontal / 2), y + (lenghtVertical / 2));
+  }
 
 private:
   // x and y cords of bottom left corner
